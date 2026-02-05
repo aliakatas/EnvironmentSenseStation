51ee7f8f68c84390f269f087318a2f5965a79ec4
Aristotelis Liakatas <aristotelis.liakatas@gmail.com>
2026-02-05 09:46:55 +0000

    Updates data collection driver to include datetime in log.

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ from secrets import *
 from mc_sensing import perform_sensor_data_averaging
 from write_to_database import write_data_to_postgres
 import sys
+import datetime
 
 if __name__ == "__main__":
 
@@ -9,21 +10,28 @@ if __name__ == "__main__":
    if len(sys.argv) > 1:
       comment = sys.argv[1]
    
-   sample_data = perform_sensor_data_averaging(URL)
-   
-   # Write data to database
-   success = write_data_to_postgres(
-      host=HOST,
-      database=DATABASE,
-      user=DBUSER,
-      password=DBUSERPASS,
-      table_name=TABLENAME,
-      data_records=sample_data,
-      comment=comment,
-      port=PORT,
-   )
-   
-   if success:
-      print("Data written successfully!")
-   else:
-      print("Failed to write data.")
+   now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+   try:
+      sample_data = perform_sensor_data_averaging(URL)
+      
+      # Write data to database
+      success = write_data_to_postgres(
+         host=HOST,
+         database=DATABASE,
+         user=DBUSER,
+         password=DBUSERPASS,
+         table_name=TABLENAME,
+         data_records=sample_data,
+         comment=comment,
+         port=PORT,
+      )
+      if success:
+         print(f"Data written successfully at {now}")
+      else:
+         print(f"Failed to write data at {now}")
+
+   except Exception as e:
+      print(f"[{now}] An error occurred: {e}")
+
+   print("*************************")
+
