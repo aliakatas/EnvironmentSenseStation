ef948478fec347ed24ddcc2a8ab9b7b94fcb0c15
Aristotelis Liakatas <aristotelis.liakatas@gmail.com>
2026-07-13 20:24:01 +0100

    Adds the ability to get data from more than one sensors.

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from secrets import URL, URLPORT, HOST, DATABASE, DBUSER, DBUSERPASS, TABLENAME, PORT
+from secrets import SENSORS, HOST, DATABASE, DBUSER, DBUSERPASS, TABLENAME, PORT
 from mc_sensing import perform_sensor_data_averaging
 from write_to_database import write_data_to_postgres
 import sys
@@ -11,30 +11,34 @@ if __name__ == "__main__":
       comment = sys.argv[1]
    
    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-   try:
-      sample_data = perform_sensor_data_averaging(URL, URLPORT)
+   for sensor in SENSORS:
+      try:
+         sample_data = perform_sensor_data_averaging(sensor["url"], sensor["port"])
 
-      if sample_data is None:
-         raise RuntimeError("No valid data collected for averaging. Skipping database write.")
-      
-      # Write data to database
-      success = write_data_to_postgres(
-         host=HOST,
-         database=DATABASE,
-         user=DBUSER,
-         password=DBUSERPASS,
-         table_name=TABLENAME,
-         data_records=sample_data,
-         comment=comment,
-         port=PORT,
-      )
-      if success:
-         print(f"Data written successfully at {now}")
-      else:
-         print(f"Failed to write data at {now}")
+         if sample_data is None:
+            raise RuntimeError("No valid data collected for averaging. Skipping database write.")
+         
+         sample_data["location"] = sensor["location"]
+         sample_data["name"] = sensor["name"]
 
-   except Exception as e:
-      print(f"[{now}] An error occurred: {e}")
+         # Write data to database
+         success = write_data_to_postgres(
+            host=HOST,
+            database=DATABASE,
+            user=DBUSER,
+            password=DBUSERPASS,
+            table_name=TABLENAME,
+            data_records=sample_data,
+            comment=comment,
+            port=PORT,
+         )
+         if success:
+            print(f"Data written successfully at {now}")
+         else:
+            print(f"Failed to write data at {now}")
+
+      except Exception as e:
+         print(f"[{now}, {sensor["name"]}] An error occurred: {e}")
 
    print("*************************")
 
